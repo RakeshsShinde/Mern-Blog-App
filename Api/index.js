3d6ed6758af860e2495dc41858eed0e774b536cd
rakesh <rakeshshinde5253@gmail.com>
2023-03-09 19:06:12 +0530

    update design

--- Api/index.js
+++ Api/index.js
@@ -6,13 +6,13 @@ const authRoute = require('./routes/auth')
 const userRoute = require('./routes/users')
 const postRoute = require('./routes/posts')
 const categoryRoute = require('./routes/categories')
-const multer=require('multer');
-const path=require('path');
+const multer = require('multer');
+const path = require('path');
 
 dotenv.config();
 
 app.use(express.json())
-app.use("/images",express.static(path.join(__dirname,"/images")))
+app.use("/images", express.static(path.join(__dirname, "/images")))
 mongoose.set('strictQuery', true)
 
 mongoose.connect(process.env.MONGO_URL, {
@@ -24,18 +24,18 @@ mongoose.connect(process.env.MONGO_URL, {
         console.log(err);
     });
 
-    const storage=multer.diskStorage({
-        destination:(req,file,cb)=>{
-            cb(null,"images")
-        },filename:(req,file,cb)=>{
-            cb(null,req.body.name)
-        }
-    })
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "images")
+    }, filename: (req, file, cb) => {
+        cb(null, req.body.name)
+    }
+})
 
-    const upload=multer({storage:storage});
-    app.post("/api/upload",upload.single("file"), (req,res)=>{
-        res.status(200).json("file has been upload !")
-    })
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("file has been upload !")
+})
 
 
 app.use("/api/auth", authRoute);
